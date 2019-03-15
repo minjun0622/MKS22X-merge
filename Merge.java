eb87f53da3ffd1503008178d20243251f707f69f
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2019-03-15 14:50:17 -0400

    warning bell commit.

--- Merge.java
+++ Merge.java
@@ -1,6 +1,39 @@
 public class Merge {
 
 /*sort the array from least to greatest value. This is a wrapper function*/
-public static void mergesort(int[]data){}
+public static void mergesort(int[] data){
+  int[] temp;
+  //copying array.
+  for (int i = 0; i < data.length; i++){
+    data[i] = temp[i];
+  }
 
+}
+
+private static void merge(int[] data, int lo, int hi) {
+  if (lo >= hi) {
+    return;
+  }
+  
+  if (data.length = 1){
+    return data[0];
+  }
+
+  int round;
+  if ( (data.length % 2) = 0) {
+    round = data.length / 2;
+  }
+  else {
+    round = data.length / 2 + 1;
+  }
+
+  merge(data[round], int lo - round, int hi);
+  merge(data[round], int lo, int hi - round);
+
+}
+public static void main(String[] args) {
+  int[] monkey = new monkey;
+
+}
+//consulting the barron's book soon.
 }
